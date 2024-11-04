7b2228b1e5c64352f4446547f45af09eacf2a10a
jerrymouse <3431193795@qq.com>
2024-11-04 23:54:57 +0800

    代码生成工具

--- src/main/java/org/jerry/code/controller/SaveTableController.java
+++ src/main/java/org/jerry/code/controller/SaveTableController.java
@@ -3,13 +3,9 @@ package org.jerry.code.controller;
 import lombok.RequiredArgsConstructor;
 import org.jerry.code.api.Result;
 import org.jerry.code.service.ISaveTableService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.annotation.Resource;
 
 @RestController
 @RequiredArgsConstructor
